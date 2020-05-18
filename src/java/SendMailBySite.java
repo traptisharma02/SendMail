1b03037935869e36c4e339484362861e1c04af19
trapti <60812549+traptisharma02@users.noreply.github.com>
2020-05-19 04:11:21 +0530

    Email

---
+++ src/java/SendMailBySite.java
@@ -0,0 +1,32 @@
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.*;
+public class SendMailBySite {
+public static void main(String[] args) {
+String to="traptisharma992@gmail.com";//change accordingly
+//Get the session object
+Properties props = new Properties();
+props.put("mail.smtp.host", "smtp.gmail.com");
+props.put("mail.smtp.socketFactory.port", "465");
+props.put("mail.smtp.socketFactory.class",
+"javax.net.ssl.SSLSocketFactory");
+props.put("mail.smtp.auth", "true");
+props.put("mail.smtp.port", "465");
+Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+protected PasswordAuthentication getPasswordAuthentication() {
+return new PasswordAuthentication("traptitrisha@gmail.com","trapti18c3192@");//Put your email id and password here
+}
+});
+//compose message
+try {
+MimeMessage message = new MimeMessage(session);
+message.setFrom(new InternetAddress("traptitrisha@gmail.com"));//change accordingly
+message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));
+message.setSubject("Hello");
+message.setText("Testing.......");
+//send message
+Transport.send(message);
+System.out.println("message sent successfully");
+} catch (MessagingException e) {throw new RuntimeException(e);}
+}
+}
